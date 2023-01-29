5e1c6d8969b6625310de4183c8ad2a46fb922d0b
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-01-29 21:39:09 +0530

    Time: 33 ms (43.16%), Space: 19.4 MB (16.89%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,29 +1,46 @@
 class Solution {
 public:
-   void moveZeroes(vector<int>& nums) {
-    int n = nums.size();
+//    void moveZeroes(vector<int>& nums) {
+//     int n = nums.size();
 
 
-    int numZeroes = 0;
-    for (int i = 0; i < n; i++) {
-        numZeroes += (nums[i] == 0);
-    }
+//     int numZeroes = 0;
+//     for (int i = 0; i < n; i++) {
+//         numZeroes += (nums[i] == 0);
+//     }
 
 
-    vector<int> ans;
-    for (int i = 0; i < n; i++) {
-        if (nums[i] != 0) {
-            ans.push_back(nums[i]);
-        }
-    }
+//     vector<int> ans;
+//     for (int i = 0; i < n; i++) {
+//         if (nums[i] != 0) {
+//             ans.push_back(nums[i]);
+//         }
+//     }
 
-    while (numZeroes--) {
-        ans.push_back(0);
-    }
+//     while (numZeroes--) {
+//         ans.push_back(0);
+//     }
 
  
-    for (int i = 0; i < n; i++) {
-        nums[i] = ans[i];
+//     for (int i = 0; i < n; i++) {
+//         nums[i] = ans[i];
+//     }
+       
+// }
+    void moveZeroes(vector<int>& nums) {
+    int lastNonZeroFoundAt = 0;
+    // If the current element is not 0, then we need to
+    // append it just in front of last non 0 element we found. 
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) {
+            nums[lastNonZeroFoundAt++] = nums[i];
+        }
+    }
+ 	// After we have finished processing new elements,
+ 	// all the non-zero elements are already at beginning of array.
+ 	// We just need to fill remaining array with 0's.
+    for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
+        nums[i] = 0;
     }
 }
 };
