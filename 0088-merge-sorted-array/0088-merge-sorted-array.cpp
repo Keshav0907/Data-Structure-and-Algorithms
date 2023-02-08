73ca7b363b642262443d2036be31531bda64ef49
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-02-08 16:11:00 +0530

    Time: 0 ms (100.00%), Space: 9 MB (75.12%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,33 +1,55 @@
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+//         vector<int> aux;
+        
+//         int i=0;
+//         int j=0;
+        
+//         while(i < m && j < n) {
+//             if(nums1[i] <= nums2[j]) {
+//                 aux.push_back(nums1[i]);
+//                 i++;
+//             } else {
+//                 aux.push_back(nums2[j]);
+//                 j++;
+//             }
+//         }
+        
+//         while(i < m) {
+//             aux.push_back(nums1[i++]);
+//         }
+        
+//         while(j < n) {
+//             aux.push_back(nums2[j++]);
+//         }
+        
+    
+//         for(int i=0; i<aux.size(); i++) {
+//             nums1[i] = aux[i];
+//         }
+        
+//     }
+// };
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        vector<int> aux;
-        
-        int i=0;
-        int j=0;
-        
-        while(i < m && j < n) {
-            if(nums1[i] <= nums2[j]) {
-                aux.push_back(nums1[i]);
-                i++;
-            } else {
-                aux.push_back(nums2[j]);
-                j++;
-            }
+
+        int i=m-1 , j=n-1 ;
+        while(i>=0 && j>=0 ){
+            if(nums1[i]>=nums2[j]){
+                nums1[i+j+1]=nums1[i] ;
+                i-- ;
+                }else{
+                    nums1[i+j+1]=nums2[j];
+                    j-- ;
+                }
         }
-        
-        while(i < m) {
-            aux.push_back(nums1[i++]);
-        }
-        
-        while(j < n) {
-            aux.push_back(nums2[j++]);
-        }
-        
-    
-        for(int i=0; i<aux.size(); i++) {
-            nums1[i] = aux[i];
+        while(j>=0){
+                    nums1[j]=nums2[j];
+                    j-- ;
         }
         
     }
