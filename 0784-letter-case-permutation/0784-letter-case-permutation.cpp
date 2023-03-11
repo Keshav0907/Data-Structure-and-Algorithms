ef592ab9db45effff42009687d0a8fe44b98844f
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-03-11 23:36:14 +0530

    Time: 11 ms (34.48%), Space: 11.4 MB (50.60%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -1,35 +1,70 @@
 class Solution {
 public:
-    vector<string> str;
-    unordered_map<string,int> mpp;
-    void helper(string s,int i) {
+    
+//     vector<string> str;
+//     unordered_map<string,int> mpp;
+//     void helper(string s,int i) {
         
-        if(i==s.length()) {
-            // str.push_back(s);
-            mpp[s]++;
+//         if(i==s.length()) {
+//             // str.push_back(s);
+//             mpp[s]++;
+//             return;
+//         }
+        
+//         // string s1;
+//         if(isalpha(s[i])) {
+//              s[i] = toupper(s[i]);
+//         }
+        
+//         helper(s,i+1);
+//         // string s2;
+//           if(isalpha(s[i])) {
+//              s[i] = tolower(s[i]);
+//         }
+//         helper(s,i+1);
+        
+//     }
+    
+    void solve(string ip,string op,vector<string> &str) {
+        
+        if(ip.length()==0) {
+            str.push_back(op);
             return;
         }
         
-        // string s1;
-        if(isalpha(s[i])) {
-             s[i] = toupper(s[i]);
+        if(isalpha(ip[0])) {
+            string op1 = op;
+            string op2 = op;
+            op1.push_back(tolower(ip[0]));
+            op2.push_back(toupper(ip[0]));
+            ip.erase(ip.begin()+0);
+            solve(ip,op1,str);
+            solve(ip,op2,str);
+        } else {
+            string op1 = op;
+            op1.push_back(ip[0]);
+            ip.erase(ip.begin()+0);
+            solve(ip,op1,str);            
         }
         
-        helper(s,i+1);
-        // string s2;
-          if(isalpha(s[i])) {
-             s[i] = tolower(s[i]);
-        }
-        helper(s,i+1);
         
     }
     
     vector<string> letterCasePermutation(string s) {
         
-        helper(s,0);
-        for(auto mp : mpp) {
-            str.push_back(mp.first);
-        }
+        // helper(s,0);
+        // for(auto mp : mpp) {
+        //     str.push_back(mp.first);
+        // }
+        // return str;
+        
+        vector<string> str;
+        string ip = s;
+        string o = "";
+        
+        solve(s,o,str);
+        
+        
         return str;
         
         
