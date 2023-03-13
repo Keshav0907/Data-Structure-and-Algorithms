4ec2fe529a0c04e794bb555bc206c7fd3b230b79
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-03-13 23:16:48 +0530

    Time: 7 ms (21.49%), Space: 15.3 MB (36.54%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     
-    void solve(vector<int> nums,int i,vector<vector<int>> &res,vector<int> op)    {
+    void solve(vector<int> nums,int i,vector<vector<int>> &res,vector<int> &op)    {
         
         if(i==nums.size()) {
             res.push_back(op);
             return;
         }
         
-        vector<int> op1 = op;
-        vector<int> op2 = op;
-        op2.push_back(nums[i]);
-        solve(nums,i+1,res,op1);
-        solve(nums,i+1,res,op2);
+        // vector<int> op1 = op;
+        // vector<int> op2 = op;
+        op.push_back(nums[i]);
+        solve(nums,i+1,res,op);
+        op.pop_back();
+        solve(nums,i+1,res,op);
         
        
     }
