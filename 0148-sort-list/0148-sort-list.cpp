dac5dc86e48aa4dcae543264294c3e5ca05388bc
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-04-17 11:09:10 +0530

    Time: 202 ms (64.82%), Space: 53.3 MB (71.19%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -26,51 +26,51 @@ public:
         
     }
     
-//     ListNode* merge(ListNode* a,ListNode* b) {
+    ListNode* merge(ListNode* a,ListNode* b) {
         
-//         if(a==NULL) {
-//             return b;
-//         } else if(b==NULL) {
-//             return a;
-//         }
-        
-//         ListNode* temp;
-        
-//         if(a->val <= b->val) {
-//             temp = a;
-//             temp->next = merge(a->next,b);
-//         } else {
-//             temp = b;
-//             temp->next = merge(a,b->next);
-//         }
-        
-//          return temp;
-        
-//     }
-    
-      ListNode* merge(ListNode* a,ListNode* b) {
-        
-        ListNode* dummy = new ListNode(-1);
-        ListNode* curr = dummy;
-        while(a && b) {
-            
-            if(a->val <= b->val) {
-                dummy->next = a;
-                a = a->next;
-            } else {
-                dummy->next = b;
-                b = b->next;
-            }
-            
-            dummy = dummy->next;
-            
+        if(a==NULL) {
+            return b;
+        } else if(b==NULL) {
+            return a;
         }
         
-        if(a) dummy->next = a;
-        if(b) dummy->next = b;
+        ListNode* temp;
         
-        return curr->next;
-      }
+        if(a->val <= b->val) {
+            temp = a;
+            temp->next = merge(a->next,b);
+        } else {
+            temp = b;
+            temp->next = merge(a,b->next);
+        }
+        
+         return temp;
+        
+    }
+    
+//       ListNode* merge(ListNode* a,ListNode* b) {
+        
+//         ListNode* dummy = new ListNode(-1);
+//         ListNode* curr = dummy;
+//         while(a && b) {
+            
+//             if(a->val <= b->val) {
+//                 dummy->next = a;
+//                 a = a->next;
+//             } else {
+//                 dummy->next = b;
+//                 b = b->next;
+//             }
+            
+//             dummy = dummy->next;
+            
+//         }
+        
+//         if(a) dummy->next = a;
+//         if(b) dummy->next = b;
+        
+//         return curr->next;
+//       }
     
     ListNode* sortList(ListNode* head) {
         
@@ -90,61 +90,3 @@ public:
 };
 
 
-// class Solution {
-// public:
-//     ListNode* sortList(ListNode* head) {
-//         if (!head || !head->next)
-//             return head;
-//         ListNode* mid = getMid(head);
-//         ListNode *right = mid->next;
-//         mid->next = NULL;
-//         ListNode* left = sortList(head);
-//         right = sortList(mid);
-//         return merge(left, right);
-//     }
-
-//     ListNode* merge(ListNode* list1, ListNode* list2) {
-//         ListNode dummyHead(0);
-//         ListNode* ptr = &dummyHead;
-//         while (list1 && list2) {
-//             if (list1->val < list2->val) {
-//                 ptr->next = list1;
-//                 list1 = list1->next;
-//             } else {
-//                 ptr->next = list2;
-//                 list2 = list2->next;
-//             }
-//             ptr = ptr->next;
-//         }
-//         if(list1) ptr->next = list1;
-//         else ptr->next = list2;
-
-//         return dummyHead.next;
-//     }
-
-//     // ListNode* getMid(ListNode* head) {
-//     //     ListNode* midPrev = nullptr;
-//     //     while (head && head->next) {
-//     //         midPrev = (midPrev == nullptr) ? head : midPrev->next;
-//     //         head = head->next->next;
-//     //     }
-//     //     ListNode* mid = midPrev->next;
-//     //     midPrev->next = nullptr;
-//     //     return mid;
-//     // }
-    
-//         ListNode* getMid(ListNode* head) {
-        
-//         ListNode* slow = head;
-//         ListNode* fast = head->next;
-        
-//         while(fast && fast->next) {
-//             slow = slow->next;
-//             fast = fast->next;
-//         }
-        
-//         return slow;
-        
-        
-//     }
-// };
