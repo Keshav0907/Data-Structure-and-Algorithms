c8756e19cf04c7333c0f81b9e1aecc09715e029c
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-04-23 22:16:48 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.cpp
+++ Next Greater Element - GFG/next-greater-element.cpp
@@ -7,60 +7,31 @@ using namespace std;
 class Solution
 {
     public:
-    //Function to find the next greater element for each element of the array.
-    vector<long long> nextLargerElement(vector<long long> arr, int n){
-        
-        // stack<long long> s;
-        // vector<long long> ans;
-        // for(int i=n-1; i>=0; i--) {
-            
-        //     if(s.size()==0) {
-        //         ans.push_back(-1);
-        //     } else if(s.size()>0 && s.top()>arr[i]) {
-        //         ans.push_back(s.top());
-        //     } else if(s.size()>0 && s.top()<=arr[i]) {
-        //         while(s.size()>0 && s.top()<=arr[i]) {
-        //             s.pop();
-        //         }
-        //         if(s.size()==0) {
-        //             ans.push_back(-1);
-        //         } else {
-        //             ans.push_back(s.top());
-        //         }
-        //     }
-            
-        //     s.push(arr[i]);
-        // }
-        
-        // reverse(ans.begin(),ans.end());
-        
-        // Your code here
-           vector<long long> v;
+     vector<long long> nextLargerElement(vector<long long> a, int n){    
         stack<long long> s;
-        int i=n-1;
-        while(i>=0)
-        {
+        vector<long long> ans;
+      
+        
+        for(int i=n-1;i>=0;i--){
+            
+            while(!s.empty() && a[i]>=s.top()){
+                s.pop();
+            }
+            
             if(s.empty())
-            {
-                v.push_back(-1);
-                s.push(arr[i]);
-                i--;
-            }
+                ans.push_back(-1);
             else
-            {
-                if(s.top()>arr[i])
-                {
-                    v.push_back(s.top());
-                    s.push(arr[i]);
-                    i--;
-                }
-                else
-                    s.pop();
-            }
+                ans.push_back(s.top());
+                
+            s.push(a[i]);
+            
         }
-        reverse(v.begin(),v.end());
-        return v;
+        
+        reverse(ans.begin(),ans.end());
+        
+        return ans;
     }
+    
     
     
 };
