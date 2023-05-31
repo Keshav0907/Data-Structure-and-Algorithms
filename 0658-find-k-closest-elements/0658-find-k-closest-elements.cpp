a2f479c4b72b8384539cd6497c870b33e5a38b24
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-05-31 22:27:53 +0530

    Time: 47 ms (66.15%), Space: 31 MB (97.72%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -2,23 +2,30 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         
-        priority_queue<pair<int,int>> maxh;
-        vector<int> ans;
-        for(int i=0; i<arr.size(); i++) {
+//         priority_queue<pair<int,int>> maxh;
+//         vector<int> ans;
+//         for(int i=0; i<arr.size(); i++) {
             
-            int ele = abs(arr[i] - x);
-            maxh.push({ele,arr[i]});
-            if(maxh.size()>k) {
-                maxh.pop();
-            }
-        }
+//             int ele = abs(arr[i] - x);
+//             maxh.push({ele,arr[i]});
+//             if(maxh.size()>k) {
+//                 maxh.pop();
+//             }
+//         }
         
-        while(maxh.size()!=0) {
-            ans.push_back(maxh.top().second);
-            maxh.pop();
-        }
+//         while(maxh.size()!=0) {
+//             ans.push_back(maxh.top().second);
+//             maxh.pop();
+//         }
         
-        sort(ans.begin(),ans.end());
-        return ans;
+//         // sort(ans.begin(),ans.end());
+//         return ans;
+        
+        int L = 0, R = size(arr)-1;
+        while (R - L >= k) 
+            if (x - arr[L] <= arr[R] - x) R--;
+            else L++;
+        return vector<int>(begin(arr) + L, begin(arr) + R + 1);
+    
     }
 };
