0c9952b52bea786840dbfed37f63d5b9cf306c26
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-06-16 20:19:19 +0530

    Time: 639 ms (73.24%), Space: 56.6 MB (76.06%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -1,50 +1,50 @@
-// class Solution {
-// public:
-//     int minimumDeviation(vector<int>& nums) {
-        
-//         priority_queue<int> maxH;
-        
-//         int diff = INT_MAX;
-//         int mini = INT_MAX;
-        
-//         for(auto x : nums) {
-//             if(x % 2 != 0) x*=2;
-//             mini = min(x,mini);
-//             maxH.push(x);
-//         }
-        
-//         while(maxH.top()%2==0) {
-//             int ele = maxH.top();
-//             maxH.pop();
-//             diff = min(diff,ele - mini);
-//             mini = min(mini,ele/2);
-//             maxH.push(ele/2);
-//         }
-        
-//         return min(diff,maxH.top()-mini);
-        
-//     }
-// };
-
 class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
-        priority_queue<int> maxpq; //max heap
-        int m = INT_MAX, diff = INT_MAX;
-        for(auto i : nums){
-            if(i%2 != 0) i*=2;
-            m = min(m, i);
-            maxpq.push(i);
+        
+        priority_queue<int> maxH;
+        
+        int diff = INT_MAX;
+        int mini = INT_MAX;
+        
+        for(auto x : nums) {
+            if(x% 2!= 0) x*=2;
+            mini = min(x,mini);
+            maxH.push(x);
         }
         
-        while(maxpq.top()%2 == 0) {
-            int mx = maxpq.top();
-            maxpq.pop();
-            diff = min(diff, mx - m);
-            m = min(m, mx/2);
-            maxpq.push(mx/2);
+        while(maxH.top()%2==0) {
+            int ele = maxH.top();
+            maxH.pop();
+            diff = min(diff,ele - mini);
+            mini = min(mini,ele/2);
+            maxH.push(ele/2);
         }
-    
-        return min(diff, maxpq.top() - m);
+        
+        return min(diff,maxH.top()-mini);
+        
     }
 };
+
+// class Solution {
+// public:
+//     int minimumDeviation(vector<int>& nums) {
+//         priority_queue<int> maxpq; //max heap
+//         int m = INT_MAX, diff = INT_MAX;
+//         for(auto i : nums){
+//             if(i%2 != 0) i*=2;
+//             m = min(m, i);
+//             maxpq.push(i);
+//         }
+        
+//         while(maxpq.top()%2 == 0) {
+//             int mx = maxpq.top();
+//             maxpq.pop();
+//             diff = min(diff, mx - m);
+//             m = min(m, mx/2);
+//             maxpq.push(mx/2);
+//         }
+    
+//         return min(diff, maxpq.top() - m);
+//     }
+// };
