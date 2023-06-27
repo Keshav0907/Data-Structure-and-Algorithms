290be09dc8581ec5af4cd37b4c45375f309a09c5
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-06-27 13:47:11 +0530

    Time: 3 ms (96.69%), Space: 8.5 MB (34.20%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,38 +1,38 @@
-// class Solution {
-// public:
-   
-//     int numOfsetbits(int n) {
-//     int ans = 0;
-//       while(n > 0) {
-//           n = (n)&(n-1);
-//           ans++;
-//       }
-    
-//      return ans;
-
-//     }
-
-//     vector<int> countBits(int n) {
-
-//         vector<int> ans;
-
-//         for(int i=0; i<=n; i++) {
-//             int result = numOfsetbits(i);
-//             ans.push_back(result);
-//         }
-
-//       return ans;
-        
-//     }
-// };
-
 class Solution {
 public:
-    vector<int> countBits(int num) {
-        vector<int> ret(num+1, 0);
-        for ( int i = 1; i <= num; i++ ) {
-            ret[i] = ret[i/2] + i%2;
+   
+    int numOfsetbits(int n) {
+    int ans = 0;
+      while(n > 0) {
+          n = (n)&(n-1);
+          ans++;
+      }
+    
+     return ans;
+
+    }
+
+    vector<int> countBits(int n) {
+
+        vector<int> ans;
+
+        for(int i=0; i<=n; i++) {
+            int result = numOfsetbits(i);
+            ans.push_back(result);
         }
-        return ret;
+
+      return ans;
+        
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> countBits(int num) {
+//         vector<int> ret(num+1, 0);
+//         for ( int i = 1; i <= num; i++ ) {
+//             ret[i] = ret[i/2] + i%2;
+//         }
+//         return ret;
+//     }
+// };
