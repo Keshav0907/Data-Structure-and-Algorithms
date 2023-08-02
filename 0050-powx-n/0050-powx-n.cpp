84dfb22c7d5f12bb2e51f61a03feb3bdc8df80a8
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-08-02 17:51:00 +0530

    Time: 0 ms (100.00%), Space: 6 MB (57.58%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,68 +1,29 @@
-// class Solution {
-// public:
-    
-//     double helper(double x, int n) {
-        
-//         double subProb = myPow(x,n/2);
-//         double subProbsq = subProb*subProb;
-            
-//         if(n%2==0) {
-//                 return subProbsq;
-//          } 
-     
-//          return x*subProbsq;
-                 
-//     }
-    
-//     double myPow(double x, int n) {
-        
-//         // double multiplier = 1;
-//         // if(x < 0 && n%2==1) {
-//         //     cout << "hello";
-//         //     multiplier = -1;
-//         // }    
-//         if(n==0) {
-//             return 1;
-//         }
-//         int result;
-//         if(n > 0) {
-           
-//           if(x<0 && n%2==1) {
-//              return -1*helper(-x,n);  
-//           } else {
-//               return helper(x,n);
-//           }
-         
-            
-//         } 
-            
-//         if(x<0 && n%2==1) {
-//             return -1.00/helper(-x,-n);
-//         }    
-            
-//         return 1.00/helper(x,-n);    
-        
-        
-//     }
-// };
-
 class Solution {
 public:
-    double myPow(double x, int n) {
-        if(n==0)
-        return 1;
-        if(n==1)
-        return x;
-        double ans = myPow(x,n/2);
-        if(n%2==0){
-            return ans*ans;
+    double binaryExp(double x, long long n) {
+        if (n == 0) {
+            return 1;
         }
-        else if(n%2==1){
-            return x*ans*ans;
+       
+        // Perform Binary Exponentiation.
+        double result = 1;
+        long long N = n;
+        if(n < 0) N = -N;
+        while (N) {
+            // If 'n' is odd we multiply result with 'x' and reduce 'n' by '1'.
+            if ( N % 2 == 1) {
+                result = result * x;
+                N -= 1;
+            }
+            // We square 'x' and reduce 'n' by half, x^n => (x^2)^(n/2).
+            x = x * x;
+            N = N / 2;
         }
-        else{
-            return 1/myPow(x,-n);
-        }
+        if(n < 0) return (double)1/(double)result;
+        return result;
+    }
 
+    double myPow(double x, int n) {
+        return binaryExp(x, (long long) n);
     }
 };
