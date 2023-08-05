d90fd615796a8718493db21edc2ac4bc365737ba
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-08-05 16:19:52 +0530

    Time: 13 ms (92.34%), Space: 17.9 MB (37.44%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-      int ans =0;
-      for(int i = 0; i < nums.size(); i++){
-          ans = ans^nums[i]^i;
-      } 
-      ans ^= nums.size(); 
-      return ans;
+         
+           int i =0;
+           while(i<nums.size()) {
+               
+            int correctindex=nums[i];
+            
+            if(nums[i] < nums.size() && i!=correctindex) {
+                swap(nums[i],nums[correctindex]);
+            } else{
+                i++;
+            }
+            
+        }
+        
+        for(int index=0; index<nums.size(); index++) {
+            if(nums[index]!=index){
+                return index;
+            }
+        }
+        
+        
+        return nums.size();
     }
 };
