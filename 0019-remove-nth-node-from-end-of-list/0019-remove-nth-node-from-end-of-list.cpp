bbad0e0720c771c48f907622b276800cf8630667
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-08-05 11:31:30 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (9.73%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,82 +8,29 @@
 //  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
 //  * };
 //  */
-// class Solution {
-// public:
-    
-//     int length(ListNode* head) {
-        
-//         int cnt = 0;
-//         ListNode* temp = head;
-//         while(temp!=NULL) {
-//             cnt++;
-//             temp = temp->next;
-//         }
-        
-//         return cnt;
-        
-//     }
-//     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        
-//         ListNode* temp1 = head;
-//         int i = 1;
-//         while(i <= n) {
-//             temp1 = temp1->next;
-//             i++;
-//         }
-        
-//         ListNode* temp2 = head;
-//         if(temp1==NULL) {
-//             head=head->next;
-//             return head;
-//         }
-//         while(temp1->next!=NULL) {
-//             temp2 = temp2->next;
-//             temp1 = temp1->next;
-//         }
-        
-//         temp2->next = temp2->next->next;
-//         return head;
-        
-//         // int l = length(head);
-//         // int k = l - n;
-        
-//         // if(k==0) {
-//         //     head=head->next;
-//         //     return head;
-//         // }
-  
-//         // ListNode* temp = head;
-//         // int i=1;
-//         // while(i < k) {
-//         //     temp = temp->next;
-//         //     i++;
-//         // }
-//         // cout << temp->val;
-//         // temp->next = temp->next->next;
-//         // return head;
-//     }
-// };
+
 
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode * start = new ListNode();
-        start -> next = head;
-        ListNode* fast = start;
-        ListNode* slow = start;     
-
-        for(int i = 1; i <= n; ++i)
-            fast = fast->next;
-    
-        while(fast->next != NULL)
-        {
-            fast = fast->next;
-            slow = slow->next;
+        
+        ListNode* curr = head;
+        
+        for(int i=0; i<n; i++) {
+            curr = curr->next;
         }
         
-        slow->next = slow->next->next;
+        if(curr==NULL) return head->next;
         
-        return start->next;
+        ListNode* follower = head;
+        while(curr!=NULL && curr->next!=NULL) {
+            curr = curr->next;
+            follower = follower->next;
+        }
+        
+        follower->next = follower->next->next;
+        
+        return head;
+        
     }
 };
