af595f0b1c0b9445d4284a377f82b14ac4ef25b7
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-08-21 20:15:35 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (73.95%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    int solve(string &s,int i,vector<int> &dp) {
+        
+        if(i>=s.size()) return 1;
+        if(s[i]=='0') return 0;
+        if(i==s.size()-1) return 1;
+        if(dp[i]!=-1) return dp[i];
+        if(s[i]=='1' || (s[i]=='2' && (s[i+1]>=48 && s[i+1]<=54)))
+            return dp[i] = solve(s,i+1,dp)+solve(s,i+2,dp);
+        else
+            return dp[i] = solve(s,i+1,dp);
+        
+    }
+    
+    int numDecodings(string s) {
+        vector<int> dp(s.size(),-1);
+        return solve(s,0,dp);
+        
+    }
+};
