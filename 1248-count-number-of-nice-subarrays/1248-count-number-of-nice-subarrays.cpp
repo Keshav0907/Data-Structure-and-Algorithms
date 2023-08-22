42f69e868f001f9036266d81137021937d1eb00b
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-08-22 11:44:15 +0530

    Time: 181 ms (32.94%), Space: 90.5 MB (19.29%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -17,25 +17,55 @@
 // };
 class Solution {
 public:
-    int subArray(vector<int>& nums, int k) {
-        int count = 0, ans = 0, start = 0, end = 0;
-        int n = nums.size();
-        while(end<n){
-            if(nums[end]%2==1){
-                count++;
+    
+     int subarraySum(vector<int>& nums, int k) {
+        
+        
+        unordered_map<int,int> mpp;
+        mpp[0]=1;        
+        int count = 0;
+        int sum = 0;
+        for(int i=0; i<nums.size(); i++) {
+            
+            sum += nums[i];
+            
+            if(mpp.find(sum-k)!=mpp.end()) {
+                count += mpp[sum-k];
             }
-            while(count>k){
-                if(nums[start]%2==1){
-                    count--;
-                }
-                start++;
-            }
-            ans += end-start+1;
-            end++;
+            
+            mpp[sum]++;
+            
         }
-        return ans;
+        
+        return count;
     }
+    // int subArray(vector<int>& nums, int k) {
+    //     int count = 0, ans = 0, start = 0, end = 0;
+    //     int n = nums.size();
+    //     while(end<n){
+    //         if(nums[end]%2==1){
+    //             count++;
+    //         }
+    //         while(count>k){
+    //             if(nums[start]%2==1){
+    //                 count--;
+    //             }
+    //             start++;
+    //         }
+    //         ans += end-start+1;
+    //         end++;
+    //     }
+    //     return ans;
+    // }
     int numberOfSubarrays(vector<int>& nums, int k) {
-      return subArray(nums, k) - subArray(nums, k - 1);
+        
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i]&1) nums[i]=1;
+            else nums[i]=0;
+        }
+        
+        return subarraySum(nums,k);
+      // return subArray(nums, k) - subArray(nums,k-1);
+        
     }
 };
