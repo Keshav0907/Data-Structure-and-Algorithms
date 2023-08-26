0c556f4cf915649c7a5020ded10e0efe634022f0
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-08-26 10:11:08 +0530

    Time: 29 ms (79.95%), Space: 13.3 MB (48.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -37,6 +37,34 @@ private:
         
     }
     
+    void dfs(int row,int col,vector<vector<int>> &visited,vector<vector<char>> &grid) {
+        
+        if(row >= grid.size() || row<0 || col>=grid[0].size() || col<0) {
+            return;
+        }
+        
+        if(visited[row][col] || grid[row][col]=='0') {
+            return;
+        }
+        
+        visited[row][col]=1;
+        
+        int dx[] = {1,0,-1,0};
+        int dy[] = {0,1,0,-1};
+        
+        for(int i=0; i<4; i++) {
+            
+            int nr = row + dx[i];
+            int nc = col + dy[i];
+            
+            if(nr>=0 && nc>=0 && nr<grid.size() && nc<grid[0].size() && grid[nr][nc]=='1' && !visited[nr][nc]) {
+                dfs(nr,nc,visited,grid);
+            }
+            
+        }
+        
+    }
+    
 public:
     int numIslands(vector<vector<char>>& grid) {
         
@@ -50,13 +78,24 @@ public:
         int cnt = 0;
 
         
-        for(int i=0;i<n;i++){
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         // Unvisited node + 1 = cnt++ by any traversal
+        //         if(grid[i][j] =='1' && !vis[i][j]){
+        //             //BFS
+        //             cnt++;
+        //             bfs(i,j,vis,grid);
+        //         }
+        //     }
+        // }
+        
+           for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 // Unvisited node + 1 = cnt++ by any traversal
                 if(grid[i][j] =='1' && !vis[i][j]){
                     //BFS
                     cnt++;
-                    bfs(i,j,vis,grid);
+                    dfs(i,j,vis,grid);
                 }
             }
         }
