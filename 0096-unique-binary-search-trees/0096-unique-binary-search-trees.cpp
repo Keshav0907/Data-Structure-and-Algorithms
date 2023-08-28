9d6d25e19813dc0f158ad5a6ce29c63679898351
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-08-28 10:14:46 +0530

    Time: 2 ms (56.69%), Space: 6 MB (78.86%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -10,17 +10,35 @@ public:
         int ans = 0;
         
         for(int i=1; i<=n; i++) {
-            ans += solve(i-1,dp)*solve(n-i,dp);
+            ans += solve(i-1,dp)*solve(n-i,dp);  // no. of ways if i is the root node
         }
         
         return dp[n]=ans;
         
     }
     
+    int solve2(int n) {
+        vector<int> dp(n+1);
+        dp[0]=dp[1]=1;
+        
+        for(int i=2; i<=n; i++) {
+            
+            for(int j=1; j<=i; j++) {
+                dp[i] += dp[j-1]*dp[i-j];
+            }
+            
+        }
+        
+        return dp[n];
+        
+    }
+    
+    
     int numTrees(int n) {
      
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return solve(n,dp);
+        return solve2(n);
         
     }
 };
