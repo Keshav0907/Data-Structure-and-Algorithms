bce20b577934c81259a5d2d68dc4e92039d6cc5a
Keshav0907 <79708549+Keshav0907@users.noreply.github.com>
2023-09-06 11:44:35 +0530

    Time: 44 ms (55.56%), Space: 20.4 MB (13.84%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -6,33 +6,56 @@ public:
         if(n==1) return 1;
         
         int result = 0;
-            
+        
+        
         for(int i=0; i<n; i++) {
             
-            for(int j=i+1; j<n; j++) {
-                int count = 2;
-                int dx = points[j][0]-points[i][0];
-                int dy = points[j][1]-points[i][1];
+            unordered_map<double,int> mpp;
+            
+            for(int j=0; j<n; j++) {
                 
-                for(int k=0; k<n; k++) {
-                    
-                  if(k!=i && k!=j) {
-                      
-                      int dx_ = points[k][0] - points[i][0];
-                      int dy_ = points[k][1] - points[i][1];
-                      
-                      if(dy*dx_ == dx*dy_) count++;
-                      
-                  }    
-                    
-                    
-                }
+                if(j==i) continue;
                 
-                result = max(result,count);
+                auto dy = points[j][1]-points[i][1];
+                auto dx = points[j][0]-points[i][0];
                 
+                auto theta = atan2(dy,dx);
+                
+                mpp[theta]++;
+            }
+            
+            for(auto it : mpp) {
+                result = max(result,it.second+1);
             }
             
         }
+            
+//         for(int i=0; i<n; i++) {
+            
+//             for(int j=i+1; j<n; j++) {
+//                 int count = 2;
+//                 int dx = points[j][0]-points[i][0];
+//                 int dy = points[j][1]-points[i][1];
+                
+//                 for(int k=0; k<n; k++) {
+                    
+//                   if(k!=i && k!=j) {
+                      
+//                       int dx_ = points[k][0] - points[i][0];
+//                       int dy_ = points[k][1] - points[i][1];
+                      
+//                       if(dy*dx_ == dx*dy_) count++;
+                      
+//                   }    
+                    
+                    
+//                 }
+                
+//                 result = max(result,count);
+                
+//             }
+            
+//         }
         
           return result;
     
